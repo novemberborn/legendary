a68c3318f2023c69f5a3f111afbc538cd0adc285
Mark Wubben <mark@novemberborn.net>
2012-12-31 17:16:35 +0100

    No need to keep a list of unhandled rejections

--- debug/unhandled.js
+++ debug/unhandled.js
@@ -4,7 +4,6 @@ var EventEmitter = require("events").EventEmitter;
 var Notifier = require("../promise/_Notifier");
 
 var emitter = module.exports = new EventEmitter();
-var list = [];
 
 Notifier.unhandledRejection = function(reason){
   var tracked = {
@@ -12,13 +11,11 @@ Notifier.unhandledRejection = function(reason){
     timestamp: Date.now()
   };
 
-  list.push(tracked);
   emitter.emit("unhandled", tracked);
 
   return function(){
     if(tracked){
       tracked.handled = true;
-      list.splice(list.indexOf(tracked), 1);
       emitter.emit("handled", tracked);
       tracked = null;
     }
