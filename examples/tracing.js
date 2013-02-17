141ffbde5abbfb5c1d42653950ff02eb78b5cdaa
Mark Wubben <mark@novemberborn.net>
2013-02-17 17:21:10 +0000

    Remove debug code, will be added to legendary-debug

--- examples/tracing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("../debug").logTraces();
-
-var legendary = require("../");
-
-legendary.fulfilled(42).trace("already fulfilled", { foo: "bar" });
-
-legendary.rejected(new Error()).traceRejected();
