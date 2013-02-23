1e3dd6ed51d8853b90d8384356ed6566a4ec98c0
Mark Wubben <mark@novemberborn.net>
2013-02-23 21:24:59 +0000

    Update Grunt to 0.4, tweak JSHint config

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        es5: true,
+        esnext: true,
+        globalstrict: true,
+        immed: true,
+        indent: 2,
+        lastsemic: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        node: true,
+        nonew: true,
+        undef: true,
+        unused: true,
+        quotmark: true,
+        strict: true,
+        trailing: true,
+        white: false
+      },
+
+      gruntfile: ["Gruntfile.js"],
+      lib: ["index.js", "lib/**/*.js"],
+
+      tests: {
+        options: {
+          globals: {
+            describe: true,
+            specify: true
+          }
+        },
+
+        files: {
+          src: ["tests/**/*.js"]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+};
