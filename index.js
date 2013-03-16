efb9d4a47d37148a8ce3de63e581b15a3bb1fe21
Mark Wubben <mark@novemberborn.net>
2013-03-16 19:31:56 +0000

    Refactor to implement latest promise and resolver drafts
    * Promises/A+ 1.1 with Promise Resolution Procedure
    * Promise Creation API/D with
      - `resolver` instead of `factory`
      - Errors thrown by `resolver` attempt to reject the promise
      - Ignore subseuent calls to `resolve` and `reject`
      - Remove `Promise.from`
    
    Removes support for `isThenable`.

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ exports = module.exports = require("./lib/legendary");
 exports.Promise = require("./lib/Promise");
 
 exports.when = require("./lib/when");
-exports.isThenable = require("./lib/isThenable");
 
 var Notifier = require("./lib/Notifier");
 exports.fulfilled = function(value){
-  return new Notifier().now(true, value).promise;
+  return new Notifier().settle(true, value);
 };
 
 exports.rejected = function(reason){
-  return new Notifier().now(false, reason, exports.unhandledRejection(reason)).promise;
+  return new Notifier().settle(false, reason, exports.unhandledRejection(reason));
 };
