6f88287cf6fa71562243d5a7160d1939029a1dfc
Mark Wubben <mark@novemberborn.net>
2013-03-31 18:54:33 +0100

    Implement Synchronous Inspection Draft E
    See <https://github.com/promises-aplus/synchronous-inspection-spec/issues/6>.
    
    Notable difference from the draft is that I'm using `value` instead of `fulfillmentValue` and `reason` instead of `rejectionReason`.
    
    Now using state inspection when resolving a promise.
    
    Updated expected behavior of when(), which can now short-circuit application in case the value is a fulfilled promise.

--- tests/when-test.js
+++ tests/when-test.js
@@ -3,6 +3,7 @@
 var assert = require("assert");
 
 var adapter = global.adapter;
+var pending = adapter.pending;
 var fulfilled = adapter.fulfilled;
 var rejected = adapter.rejected;
 
@@ -58,12 +59,77 @@ describe("No callbacks are passed", function(){
 });
 
 describe("If (at least) the `onFulfilled` callback is passed:", function(){
-  specify("If a promise was received, the result of passing the `onFulfilled` callback to its then() method is returned", function(done){
-    var result = when(fulfilled(2), square);
+  specify("If a pending promise was received, the result of passing the `onFulfilled` callback to its then() method is returned", function(done){
+    var unsettled = pending();
+    var result = when(unsettled.promise, square);
     assert(result instanceof Promise);
     result.then(function(value){
       assert.equal(value, 4);
       done();
+    });
+    unsettled.fulfill(2);
+  });
+
+  specify("If a rejected promise was received, and no `onRejected` callback is passed, a promise adopting its state is returned instead.", function(done){
+    var promise = rejected(sentinel);
+    var result = when(promise, square);
+    assert.notEqual(result, promise);
+    result.then(null, function(value){
+      assert.equal(value, sentinel);
+      done();
+    })
+  });
+
+  describe("If a fulfilled promise was received, its value is passed to the `onFulfilled` callback.", function(){
+    specify("If the callback throws, a rejected promise is returned with the thrown error as rejection reason.", function(done){
+      var result = when(fulfilled(sentinel), function(value){
+        throw value;
+      });
+      assert(result instanceof Promise);
+      result.then(null, function(value){
+        assert.equal(value, sentinel);
+        done();
+      });
+    });
+
+    specify("If the callback returns a value, this result is returned as-is.", function(){
+      var result = when(fulfilled(2), square);
+      assert.equal(result, 4);
+    });
+
+    specify("If the callback returns a pending promise, this result is returned as-is.", function(){
+      var promise = pending(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, promise);
+    });
+
+    specify("If the callback returns a rejected promise, this result is returned as-is.", function(){
+      var promise = rejected(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, promise);
+    });
+
+    specify("If the callback returns a fulfilled promise, its value is returned instead.", function(){
+      var promise = fulfilled(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, sentinel);
+    });
+
+    specify("If the callback returns a thenable, a promise adopting its state is returned instead.", function(done){
+      var result = when(fulfilled(sentinel), function(value){
+        return makeThenable(value);
+      });
+      assert(result instanceof Promise);
+      result.then(function(value){
+        assert.equal(value, sentinel);
+        done();
+      });
     });
   });
 
@@ -95,12 +161,28 @@ describe("If (at least) the `onFulfilled` callback is passed:", function(){
       assert.equal(result, sentinel);
     });
 
-    specify("If the callback returns a promise, this result is returned as-is.", function(){
-      var promise = fulfilled(sentinel);
-      var result = when(true, function(){
+    specify("If the callback returns a pending promise, this result is returned as-is.", function(){
+      var promise = pending(sentinel);
+      var result = when(fulfilled(sentinel), function(){
         return promise;
       });
       assert.equal(result, promise);
+    });
+
+    specify("If the callback returns a rejected promise, this result is returned as-is.", function(){
+      var promise = rejected(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, promise);
+    });
+
+    specify("If the callback returns a fulfilled promise, its value is returned instead.", function(){
+      var promise = fulfilled(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, sentinel);
     });
 
     specify("If the callback returns a thenable, a promise adopting its state is returned instead.", function(done){
@@ -117,12 +199,72 @@ describe("If (at least) the `onFulfilled` callback is passed:", function(){
 });
 
 describe("If (at least) the `onRejected` callback is passed:", function(){
-  specify("If a promise was received, the result of passing the `onRejected` callback to its then() method is returned", function(done){
-    var result = when(rejected(2), null, square);
+  specify("If a pending promise was received, the result of passing the `onRejected` callback to its then() method is returned", function(done){
+    var unsettled = pending();
+    var result = when(unsettled.promise, null, square);
     assert(result instanceof Promise);
     result.then(function(value){
       assert.equal(value, 4);
       done();
+    });
+    unsettled.reject(2);
+  });
+
+  specify("If a fulfilled promise was received, and no `onFulfilled` callback is passed, its fulfillment value is returned.", function(){
+    var result = when(fulfilled(sentinel), null, square);
+    assert.equal(result, sentinel);
+  });
+
+  describe("If a rejected promise was received, its reason is passed to the `onRejected` callback.", function(){
+    specify("If the callback throws, a rejected promise is returned with the thrown error as rejection reason.", function(done){
+      var result = when(rejected(sentinel), null, function(value){
+        throw value;
+      });
+      assert(result instanceof Promise);
+      result.then(null, function(value){
+        assert.equal(value, sentinel);
+        done();
+      });
+    });
+
+    specify("If the callback returns a value, this result is returned as-is.", function(){
+      var result = when(rejected(2), null, square);
+      assert.equal(result, 4);
+    });
+
+    specify("If the callback returns a pending promise, this result is returned as-is.", function(){
+      var promise = pending(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, promise);
+    });
+
+    specify("If the callback returns a rejected promise, this result is returned as-is.", function(){
+      var promise = rejected(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, promise);
+    });
+
+    specify("If the callback returns a fulfilled promise, its value is returned instead.", function(){
+      var promise = fulfilled(sentinel);
+      var result = when(fulfilled(sentinel), function(){
+        return promise;
+      });
+      assert.equal(result, sentinel);
+    });
+
+    specify("If the callback returns a thenable, a promise adopting its state is returned instead.", function(done){
+      var result = when(rejected(sentinel), null, function(value){
+        return makeThenable(value);
+      });
+      assert(result instanceof Promise);
+      result.then(function(value){
+        assert.equal(value, sentinel);
+        done();
+      });
     });
   });
 
