0eaf31eef9a7f3dfafa4e5e38c0edb182909c3fa
Mark Wubben <mark@novemberborn.net>
2013-05-11 16:17:54 +0100

    Implement Promise#cancel()

---
+++ tests/cancellation-test.js
@@ -0,0 +1,167 @@
+"use strict";
+
+var assert = require("assert");
+
+var Promise = require("../").Promise;
+var NeverCancellable = require("../").NeverCancellable;
+
+var adapter = require("./adapter");
+var fulfilled = adapter.fulfilled;
+var rejected = adapter.rejected;
+var pending = adapter.pending;
+
+var sentinel = {};
+
+function noop(){}
+
+describe("Cancellation", function(){
+  describe("Calling cancel()", function(){
+    specify("No-op on a fulfilled promise", function(done){
+      var promise = fulfilled(sentinel);
+      promise.cancel();
+      promise.then(function(value){
+        assert.strictEqual(value, sentinel);
+      }).then(done, done);
+    });
+
+    specify("No-op on a rejected promise", function(done){
+      var promise = rejected(sentinel);
+      promise.cancel();
+      promise.then(null, function(reason){
+        assert.strictEqual(reason, sentinel);
+      }).then(done, done);
+    });
+
+    specify("Rejects a pending promise with a CancelError", function(done){
+      var promise = pending().promise;
+      promise.cancel();
+      promise.then(null, function(reason){
+        assert(reason instanceof Error);
+        assert.equal(reason.name, "cancel");
+      }).then(done, done);
+    });
+  });
+
+  describe("onCancelled callback", function(){
+    specify("Not called when cancelling a fulfilled promise", function(){
+      var called = false;
+      new Promise(function(resolve){
+        resolve();
+        return function(){ called = true; };
+      }).cancel();
+      assert.equal(called, false);
+    });
+
+    specify("Not called when cancelling a rejected promise", function(){
+      var called = false;
+      new Promise(function(_, reject){
+        reject();
+        return function(){ called = true; };
+      }).cancel();
+      assert.equal(called, false);
+    });
+
+    specify("Called when cancelling a pending promise", function(done){
+      var called = false;
+      var promise = new Promise(function(){
+        return function(){ called = true; };
+      });
+      promise.cancel();
+      assert.equal(called, true);
+      promise.then(null, function(reason){
+        assert.equal(reason.name, "cancel");
+      }).then(done, done);
+    });
+
+    specify("Rejects pending promise when throwing", function(done){
+      var promise = new Promise(function(){
+        return function(){ throw sentinel; };
+      });
+      promise.cancel();
+      promise.then(null, function(reason){
+        assert.strictEqual(reason, sentinel);
+      }).then(done, done);
+    });
+  });
+
+  describe("Propagation", function(){
+    specify("Cancel derived promise", function(done){
+      var called = false;
+      var promise = new Promise(function(){
+        return function(){ called = true; };
+      });
+      var derived = promise.then(noop);
+      derived.cancel();
+      assert(called);
+      promise.then(null, function(reason){
+        assert.equal(reason.name, "cancel");
+        return derived.then(null, function(reason){
+          assert.equal(reason.name, "cancel");
+        });
+      }).then(done, done);
+    });
+
+    specify("Cancel enqueued callback execution", function(done){
+      var called = false;
+      var derived = fulfilled().then(function(){ called = true; });
+      derived.cancel();
+      derived.then(null, function(reason){
+        assert(called);
+        assert.equal(reason.name, "cancel");
+      }).then(done, done);
+    });
+
+    specify("Cancel state-adoption of inner promise", function(done){
+      var called = false;
+      var inner = new Promise(function(){
+        return function(){ called = true; };
+      });
+      var derived = fulfilled().then(function(){ return inner; });
+      setImmediate(function(){
+        derived.cancel();
+        assert(called);
+        inner.then(null, function(reason){
+          assert.equal(reason.name, "cancel");
+          return derived.then(null, function(reason){
+            assert.equal(reason.name, "cancel");
+          });
+        }).then(done, done);
+      });
+    });
+
+    specify("Ignore thenable assimilation when cancelled", function(done){
+      var resolveThenable;
+      var thenable = { then: function(resolvePromise){ resolveThenable = resolvePromise; } };
+      var derived = fulfilled().then(function(){ return thenable; }).then(noop);
+      setImmediate(function(){
+        derived.cancel();
+        resolveThenable();
+        derived.then(null, function(reason){
+          assert.equal(reason.name, "cancel");
+        }).then(done, done);
+      });
+    });
+
+    specify("Cancel state adoption when resolving with promise", function(done){
+      var called = false;
+      var inner = new Promise(function(){ return function(){ called = true; }; });
+      var outer = new Promise(function(resolve){ resolve(inner); });
+      outer.cancel();
+      assert(called);
+      outer.then(null, function(reason){
+        assert.equal(reason.name, "cancel");
+      }).then(done, done);
+    });
+
+    specify("Ignore thenable assimilation when resolving with thenable", function(done){
+      var resolveThenable;
+      var thenable = { then: function(resolvePromise){ resolveThenable = resolvePromise; } };
+      var outer = new Promise(function(resolve){ resolve(thenable); });
+      outer.cancel();
+      resolveThenable();
+      outer.then(null, function(reason){
+        assert.equal(reason.name, "cancel");
+      }).then(done, done);
+    });
+  });
+});
