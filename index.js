9a4df683a543263c9e719ed0d7ebee8d9bed573e
Mark Wubben <mark@novemberborn.net>
2013-08-02 18:25:26 +0100

    Collection subclass with sugar for promises-for-arrays

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 exports = module.exports = require('./lib/legendary');
 
 exports.Promise = require('./lib/promises').Promise;
+exports.Collection = require('./lib/collections').Collection;
 exports.CancellationError = require('./lib/CancellationError');
