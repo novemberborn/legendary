af2873d671c75d59106c4b2fcefc34929c83b4c3
Mark Wubben <mark@novemberborn.net>
2014-03-06 14:02:01 +0000

    Generated documentation for 78f614ee27b009b2af7537d4839c61f13d20d1dc

---
+++ toc.js
@@ -0,0 +1,231 @@
+window.files = [
+{
+  "path": "README.md.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "README.md.html",
+  "title": "Legendary",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "legendary",
+      "title": "Legendary"
+    },
+    {
+      "level": 2,
+      "slug": "installation",
+      "title": "Installation"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 2,
+      "slug": "theme-song",
+      "title": "Theme song"
+    }
+  ]
+},
+{
+  "path": "lib/legendary.js.html",
+  "originalName": "legendary.js",
+  "originalPath": "lib/legendary.js",
+  "name": "legendary.js.html",
+  "lang": "javascript",
+  "title": "Legendary",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "legendary",
+      "title": "Legendary"
+    },
+    {
+      "level": 2,
+      "slug": "promise",
+      "title": "Promise"
+    },
+    {
+      "level": 2,
+      "slug": "cancellationerror",
+      "title": "CancellationError"
+    },
+    {
+      "level": 2,
+      "slug": "unhandledrejection-reason-",
+      "title": "unhandledRejection(reason)"
+    }
+  ]
+},
+{
+  "path": "lib/promises.js.html",
+  "originalName": "promises.js",
+  "originalPath": "lib/promises.js",
+  "name": "promises.js.html",
+  "lang": "javascript",
+  "title": "Promise",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "promise",
+      "title": "Promise"
+    },
+    {
+      "level": 2,
+      "slug": "promise-isinstance-x-",
+      "title": "Promise.isInstance(x)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-from-value-",
+      "title": "Promise.from(value)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-rejected-reason-",
+      "title": "Promise.rejected(reason)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-races",
+      "title": "Promise Races"
+    },
+    {
+      "level": 3,
+      "slug": "promise-all-input-",
+      "title": "Promise.all(input)"
+    },
+    {
+      "level": 3,
+      "slug": "promise-any-input-",
+      "title": "Promise.any(input)"
+    },
+    {
+      "level": 3,
+      "slug": "promise-some-input-winsrequired-",
+      "title": "Promise.some(input, winsRequired)"
+    },
+    {
+      "level": 3,
+      "slug": "promise-join-input-",
+      "title": "Promise.join(...input)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-then-onfulfilled-onrejected-",
+      "title": "Promise#then(onFulfilled, onRejected)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-inspectstate-",
+      "title": "Promise#inspectState()"
+    },
+    {
+      "level": 2,
+      "slug": "promise-cancel-",
+      "title": "Promise#cancel()"
+    },
+    {
+      "level": 2,
+      "slug": "promise-fork-",
+      "title": "Promise#fork()"
+    },
+    {
+      "level": 2,
+      "slug": "promise-uncancellable-",
+      "title": "Promise#uncancellable()"
+    },
+    {
+      "level": 2,
+      "slug": "promise-to-constructor-",
+      "title": "Promise#to(constructor)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-trace-label-meta-",
+      "title": "Promise#trace(label, meta)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-tracefulfilled-label-meta-",
+      "title": "Promise#traceFulfilled(label, meta)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-tracerejected-label-meta-",
+      "title": "Promise#traceRejected(label, meta)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-yield-value-",
+      "title": "Promise#yield(value)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-yieldreason-reason-",
+      "title": "Promise#yieldReason(reason)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-otherwise-onrejected-",
+      "title": "Promise#otherwise(onRejected)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-ensure-onfulfilledorrejected-",
+      "title": "Promise#ensure(onFulfilledOrRejected)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-tap-onfulfilledsideeffect-onrejectedsideeffect-",
+      "title": "Promise#tap(onFulfilledSideEffect, onRejectedSideEffect)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-spread-variadiconfulfilled-",
+      "title": "Promise#spread(variadicOnFulfilled)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-nodeify-callback-",
+      "title": "Promise#nodeify(callback)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-cancelafter-milliseconds-",
+      "title": "Promise#cancelAfter(milliseconds)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-alsocancels-other-",
+      "title": "Promise#alsoCancels(other)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-send-methodname-args-",
+      "title": "Promise#send(methodName, ...args)"
+    },
+    {
+      "level": 2,
+      "slug": "promise-prop-name-",
+      "title": "Promise#prop(name)"
+    }
+  ]
+},
+{
+  "path": "lib/CancellationError.js.html",
+  "originalName": "CancellationError.js",
+  "originalPath": "lib/CancellationError.js",
+  "name": "CancellationError.js.html",
+  "lang": "javascript",
+  "title": "CancellationError",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "cancellationerror",
+      "title": "CancellationError"
+    }
+  ]
+}
+];
